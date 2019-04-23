3fd5ec1d2a5331d136e66fb84a67c2cd65254523
Ida Bergström <ida@bazooka.se>
2019-04-23 16:36:10 +0200

    Creating Custom Directives and Filters : Using Directive Lifecycle Hooks

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -21,7 +21,9 @@
     <!-- with args/modifiers: -->
     <!-- <span v-pin:position.top.right class="sale" v-show="selectedPart.onSale">Sale!</span> -->
     <!-- ...as object (more flexible): -->
-    <span v-pin="{ bottom: '10px', right: '5px'}" class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <!-- <span v-pin="{ bottom: '10px', right: '5px'}" class="sale" v-show="selectedPart.onSale">Sale!</span> -->
+    <!-- ..with directive lifecycle hooks? -->
+    <span @click="pinPadding='30px'" v-pin="{ bottom: pinPadding, right: pinPadding}" class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
@@ -55,7 +57,10 @@ export default {
     },
   },
   data() {
-    return { selectedPartIndex: 0 };
+    return {
+      selectedPartIndex: 0,
+      pinPadding: '10px',
+    };
   },
   computed: {
     selectedPart() {
--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,21 +1,46 @@
-export default {
-  // the element is the sales span-tag in this example..
-  bind: (element, binding) => {
-    // element.style.position = 'absolute';
-    // element.style.bottom = '5px';
-    // element.style.right = '5px';
+// function applyStyle(element, binding) {
+//   Object.keys(binding.value).forEach((position) => {
+//     element.style[position] = binding.value[position];
+//   });
+//   element.style.position = 'absolute';
+// }
 
-    // with binding/arg/modifiers:
-    // console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
-    // if (binding.arg !== 'position') return;
-    // Object.keys(binding.modifiers).forEach((key) => {
-    //   element.style[key] = '5px';
-    // });
-    // element.style.position = 'absolute';
+// export default {
+// the element is the sales span-tag in this example..
+// bind: (element, binding) => {
+// element.style.position = 'absolute';
+// element.style.bottom = '5px';
+// element.style.right = '5px';
 
-    Object.keys(binding.value).forEach((position) => {
-      element.style[position] = binding.value[position];
-    });
-    element.style.position = 'absolute';
-  },
-};
+// 1. with binding/arg/modifiers:
+// console.log('arg:', binding.arg, 'modifiers:', binding.modifiers);
+// if (binding.arg !== 'position') return;
+// Object.keys(binding.modifiers).forEach((key) => {
+//   element.style[key] = '5px';
+// });
+// element.style.position = 'absolute';
+
+// 2. with object sent in:
+// Object.keys(binding.value).forEach((position) => {
+//   element.style[position] = binding.value[position];
+// });
+// element.style.position = 'absolute';
+
+// 3. call function instead:
+//   applyStyle(element, binding);
+// },
+// update: (element, binding) => {
+//   applyStyle(element, binding);
+// },
+// };
+
+// Lifecycle hooks: bind, update, inserted, componentUpdated, unbind.
+
+// But bind and update mostly is used together so there's a shorthand FUNCTION for this:
+// A common way to write directives.
+export default function (element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = 'absolute';
+}
